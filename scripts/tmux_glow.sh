f1a10d9d87bd568f79efcf0ff8f0dbc19f32c530
FDIL <fdil.fadilh1@gmail.com>
2025-05-18 20:02:32 -0600

    Rewrite in bash
    Got bash a basj script working same as fish script. Changed .tmux file
    to use the bash script instead now.

--- scripts/tmux_glow.sh
+++ scripts/tmux_glow.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+glow_command="glow -tl"
 markdown_file=""
 
 # Function to set markdown_file
@@ -7,13 +8,24 @@ set_markdown_file() {
     # Look for *.md files in the current directory (no subdirectories)
     md_files=(*.md)
 
-    # Only set markdown_file if at least one markdown file is found
-    if [ ${#md_files[@]} -gt 0 ]; then
-        markdown_file="${md_files[0]}"
+    # if no .md files found, by default bash will set md_files to the string
+    # so check if the string was not set to know current directory has a md file
+    if [[ ${md_files[0]} != "*.md" ]]; then
+      markdown_file=${md_files[0]}
     fi
 }
 
+# start of flow of script
+pane_current_path=$1
+
+# change current directory of script to the current path
+cd $pane_current_path
+
 set_markdown_file
 
-# Print the selected markdown file (for debugging purposes)
-echo "Markdown file: $markdown_file"
+# create_glow_command
+glow_command="$glow_command $markdown_file"
+
+tmux splitw -h -c "$pane_current_path"
+
+tmux send -t: "$glow_command" C-m
--- tmux_glow.tmux
+++ tmux_glow.tmux
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-tmux bind g run-shell "fish $CURRENT_DIR/scripts/tmux_glow.fish #{pane_current_path}"
+tmux bind g run-shell "bash $CURRENT_DIR/scripts/tmux_glow.sh #{pane_current_path}"
